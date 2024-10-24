28e15d02df90339aeb2b85004c6e7d8105d36b64
HosseinAjerloo <ahosseinajerloo@gmail.com>
2024-10-24 09:20:22 +0330

    off voucher

--- resources/views/Panel/Purchase/index.blade.php
+++ resources/views/Panel/Purchase/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('message-box')
     <div class="flex items-center justify-center flex-col space-y-3">
-        <p class="text-center text-sm py-2 sm:text-base ">
+        <p class="text-center text-sm py-2 sm:text-base text-rose-600 font-bold">
 {{--            مبلغ ووچر پرفکت مانی درخواستی را وارد کنید یا از انتخاب سریع استفاده نمائید.--}}
         درحال بروز رسانی هستیم به زود برمیگردیم
         </p>
@@ -57,7 +57,7 @@
         <article
             class="space-y-4 flex items-center justify-center flex-col md:flex-row md:items-center w-full md:justify-around md:space-y-0 md:mt-16">
             <div class=" flex items-center justify-start  max-w-max   rounded-md wallet ">
-                <img src="{{asset('src/images/wallet.png')}}" alt="" class="w-12 h-12 bg-sky-500 rounded-md">
+{{--                <img src="{{asset('src/images/wallet.png')}}" alt="" class="w-12 h-12 bg-sky-500 rounded-md">--}}
 {{--                <button class="bg-sky-500 py-1.5 px-2 rounded-se-md rounded-ee-md">پرداخت با کیف پول</button>--}}
             </div>
 {{--            @foreach($banks as $bank)--}}
