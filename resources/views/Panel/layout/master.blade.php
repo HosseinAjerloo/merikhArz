0b59ba9fd2886bf6f2b17849de57fcfdab06fc57
HosseinAjerloo <ahosseinajerloo@gmail.com>
2024-11-23 12:20:09 +0330

    change icon iuser left menu

--- resources/views/Panel/layout/master.blade.php
+++ resources/views/Panel/layout/master.blade.php
@@ -1,5 +1,5 @@
 @include('Panel.layout.head')
-<body class="font-yekan text-white bg-gray-950  overflow-x-hidden  bg-gradient-to-t to-white-500 from-sky-500   h-screen    ">
+<body class="font-yekan text-white bg-gray-950  overflow-x-hidden  bg-gradient-to-t to-white-500 from-sky-500   h-max   ">
 @include('Panel.layout.header')
 @yield('header-tag')
 @include('Panel.layout.script')
