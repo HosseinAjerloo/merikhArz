2fefab1ef22c2fe2286427af596ee47be16a8c89
HosseinAjerloo <ahosseinajerloo@gmail.com>
2025-01-20 14:32:55 +0330

    add feature mr karimi

--- database/migrations/2024_07_01_004___create_table__site_service.php
+++ database/migrations/2024_07_01_004___create_table__site_service.php
@@ -13,9 +13,15 @@ return new class extends Migration
     {
         Schema::create('site_services', function (Blueprint $table) {
             $table->id();
-            $table->string('siteName')->nullable();
-            $table->string('url')->nullable();
-            $table->enum('is_active',['active','inactive'])->default('active');
+            $table->string('name')->nullable();
+            $table->string('domain')->nullable();
+            $table->string('tron_wallet')->nullable();
+            $table->string('products')->nullable();
+            $table->string('person_name')->nullable();
+            $table->string('email')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('license_image')->nullable();
+            $table->string('is_active',['active','inactive'])->default('active');
             $table->string('token')->nullable();
             $table->timestamps();
             $table->softDeletes();
