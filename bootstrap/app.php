ea6bad0ac5df9cebd364224a559d262ef25cd693
anonymous <anonymous@an.com>
2025-02-18 04:21:52 -0800

    global middleware for ip access

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->redirectGuestsTo(fn() => route('login.index'));
         $middleware->web(App\Http\Middleware\IPAccessMiddleware::class);
+
         $middleware->alias([
             'IsEmptyUserInformation' => \App\Http\Middleware\IsEmptyUserInformation::class,
             'LimitedPurchase' => \App\Http\Middleware\LimitedPurchaseMiddleware::class,
