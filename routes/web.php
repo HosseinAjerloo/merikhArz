bbf4d9158814efad80506e26e40b1b7d77524d95
HosseinAjerloo <ahosseinajerloo@gmail.com>
2025-07-19 12:52:58 +0330

    sync project

--- routes/web.php
+++ routes/web.php
@@ -132,39 +132,6 @@ Route::fallback(function () {
     abort(404);
 });
 
-Route::get('test', function () {
-    try {
-
-
-        $fastPayments = FastPayment::where('api_success', 'false')->get();
-
-        foreach ($fastPayments as $fastPayment) {
-            $response = Http::timeout(50)->withHeaders([
-                'token' => env('SAINAEX_TOKEN')
-            ])->withoutVerifying()->post(env('SAINAEX_REQUEST_REPORT_PAYMENT'),
-                [
-                    'success' => ($fastPayment->financeTransaction->payment->state) != 'finished' ? false : true,
-                    'invoice_id' => $fastPayment->pay_id,
-                    'amount' => $fastPayment->amount,
-                    'phone_number' => $fastPayment->financeTransaction->user->mobile
-                ]);
-            dump( [
-                'success' => ($fastPayment->financeTransaction->payment->state) != 'finished' ? false : true,
-                'invoice_id' => $fastPayment->pay_id,
-                'amount' => $fastPayment->amount,
-                'phone_number' => $fastPayment->financeTransaction->user->mobile
-            ]);
-            if ($response->status() == 200 and $response->successful()) {
-                $fastPayment->api_success = 'true';
-                $fastPayment->save();
-            }
-
-        }
-    } catch (\Exception $exception) {
-        Log::emergency('con not connection to request host ' . env('SAINAEX_REQUEST_REPORT_PAYMENT') . ' ' . $exception->getMessage());
-        SendAppAlertsJob::dispatch('خطا در ارتباط با مقصد وجود آمد لطفا شبکه راچک کنید')->onQueue('perfectmoney');
-    }
-});
 
 
 
